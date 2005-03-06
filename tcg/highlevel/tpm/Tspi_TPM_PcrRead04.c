2a4038e0b6063a13393220b6d3f9d3401d55b3c5
kyoder <kyoder>
2005-03-06 23:07:14 +0000

    initial add

---
+++ tcg/highlevel/tpm/Tspi_TPM_PcrRead04.c
@@ -0,0 +1,173 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2004
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_TPM_PcrRead04.c
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_TPM_PcrRead
+ *		returns TSS_SUCCESS.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect Context
+ *		Get TPM Object
+ *
+ *	Test:
+ *		Call TPM_PcrRead then if it does not succeed
+ *		Make sure that it returns the proper return codes
+ *		Print results
+ *
+ *	Cleanup:
+ *		Free memory relating to hContext
+ *		Close context
+ *
+ * USAGE
+ *      First parameter is --options
+ *                         -v or --version
+ *      Second parameter is the version of the test case to be run
+ *      This test case is currently only implemented for v1.1
+ *
+ * HISTORY
+ *      Megan Schneider, mschnei@us.ibm.com, 6/04.
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <stdio.h>
+#include <tss/tss.h>
+#include "../common/common.h"
+
+int
+main( int argc, char **argv )
+{
+	char			*version;
+
+	version = parseArgs( argc, argv );
+		// if it is not version 1.1, print error
+	if( strcmp(version, "1.1") )
+		print_wrongVersion();
+	else
+		main_v1_1();
+}
+
+int
+main_v1_1( void )
+{
+	char			*function = "Tspi_TPM_PcrRead04";
+	BYTE			*rgbPcrValue, *rgbNumPcrs;
+	UINT32			ulPcrValueLength;
+	TSS_HCONTEXT		hContext;
+	TSS_HTPM		hTPM;
+	TSS_RESULT		result;
+	UINT32			exitCode, subCapSize, numPcrs, subCap, i, j;
+
+	print_begin_test( function );
+
+		// Create Context
+	result = Tspi_Context_Create( &hContext );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Create", result );
+		print_error_exit( function, err_string(result) );
+		exit( result );
+	}
+
+		// Connect to Context
+	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Connect", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+		// Retrieve TPM object of context
+	result = Tspi_Context_GetTpmObject( hContext, &hTPM );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_GetTpmObject", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	subCap = TSS_TPMCAP_PROP_PCR;
+		// Retrieve number of PCR's from the TPM
+	result = Tspi_TPM_GetCapability( hTPM, TSS_TPMCAP_PROPERTY, sizeof(UINT32),
+					(BYTE *)&subCap, &ulPcrValueLength, &rgbNumPcrs);
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_GetTpmObject", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	if (ulPcrValueLength != sizeof(UINT32)) {
+		printf("GetCapability(TSS_TPMCAP_PROP_PCR) returns value != sizeof(UINT32)!");
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	numPcrs = *(UINT32 *)rgbNumPcrs;
+
+	for (i = 0; i < numPcrs; i++) {
+		result = Tspi_TPM_PcrRead( hTPM, i, &ulPcrValueLength, &rgbPcrValue );
+
+		printf("PCR%02u: ", i);
+		for (j = 0; j < ulPcrValueLength; j++) {
+			printf("%02x", rgbPcrValue[j] & 0xff);
+		}
+		printf("\n");
+	}
+
+	if ( result != TSS_SUCCESS )
+	{
+		if( !(checkNonAPI(result)) )
+		{
+			print_error( function, result );
+			exitCode = 1;
+		}
+		else
+		{
+			print_error_nonapi( function, result );
+			exitCode = 1;
+		}
+	}
+	else
+	{
+		print_success( function, result );
+		exitCode = 0;
+	}
+
+	print_end_test( function );
+	Tspi_Context_FreeMemory( hContext, NULL );
+	Tspi_Context_Close( hContext );
+	exit( exitCode );
+}
