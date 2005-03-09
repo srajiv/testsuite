5bc558353e720e415607921b4b0c854d5aea0ce6
kyoder <kyoder>
2005-03-09 20:34:16 +0000

    initial add.

---
+++ tcg/highlevel/tspi/Tspi_ChangeAuth05.c
@@ -0,0 +1,206 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2004
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_ChangeAuth05
+ *
+ * DESCRIPTION
+ *	This test will attempt to change the owner authorization of the TPM.
+ *	If it succeeds, the test will reset the owner password to its original state.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect
+ *		Get a handle to the TPM object
+ *		Set Secret
+ *		Create a new policy
+ *		Set a new secret for the new policy
+ *
+ *	Test:	Call ChangeAuth. If it is not a success
+ *		Make sure that it returns the proper return codes
+ *
+ *	Cleanup:
+ *		Free memory associated with the context
+ *		Close context
+ *		Print error/success message
+ *
+ * USAGE:	First parameter is
+ *			-v or --version
+ *		Second Parameter is the version of the test case to be run.
+ *		This test case is currently only implemented for 1.1
+ *
+ * HISTORY
+ *	Author:	Kent Yoder
+ *	Date:	March 2005
+ *	Email:	shpedoikal@gmail.com
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <tss/tss.h>
+#include "../common/common.h"
+
+extern TSS_UUID SRK_UUID;
+extern int commonErrors(TSS_RESULT result);
+
+int main(int argc, char **argv)
+{
+	char		*version;
+
+	version = parseArgs( argc, argv );
+		// if it is not version 1.1, print error
+	if(strcmp(version, "1.1")){
+		print_wrongVersion();
+	}
+	else{
+		main_v1_1();
+	}
+}
+
+main_v1_1(void){
+
+	char		*nameOfFunction = "Tspi_ChangeAuth05";
+	TSS_HKEY	hTPM;
+	TSS_HPOLICY	hTPMPolicy, hNewPolicy;
+	TSS_HCONTEXT	hContext;
+	TSS_RESULT	result;
+
+	print_begin_test(nameOfFunction);
+
+		//Create Context
+	result = Tspi_Context_Create(&hContext);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Create ", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		exit(result);
+	}
+		//Connect Context
+	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Connect", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+	result = Tspi_Context_GetTpmObject(hContext, &hTPM);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_GetTpmObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+	result = Tspi_GetPolicyObject(hTPM, TSS_POLICY_USAGE, &hTPMPolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_GetPolicyObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Set Secret
+	result = Tspi_Policy_SetSecret(hTPMPolicy, TSS_SECRET_MODE_PLAIN,
+			sizeof(OWN_PWD), OWN_PWD);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Create New Policy Object
+	result = Tspi_Context_CreateObject(hContext, TSS_OBJECT_TYPE_POLICY, TSS_POLICY_USAGE, &hNewPolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_CreateObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+		// Set Secret
+	result = Tspi_Policy_SetSecret(hNewPolicy, TSS_SECRET_MODE_PLAIN,
+			sizeof(KEY_PWD), KEY_PWD);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Call Change Auth
+	result = Tspi_ChangeAuth(hTPM, NULL_HKEY, hNewPolicy);
+	if (result != TSS_SUCCESS) {
+		if(!checkNonAPI(result)){
+			print_error(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(1);
+		}
+		else{
+			print_error_nonapi(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(1);
+		}
+	}
+	else{
+		printf("Success.  Resetting the owner password...\n");
+		// Change the owner secret back to what it was
+		result = Tspi_Policy_SetSecret(hNewPolicy, TSS_SECRET_MODE_PLAIN,
+				sizeof(OWN_PWD), OWN_PWD);
+		if (result != TSS_SUCCESS) {
+			print_error("Tspi_Policy_SetSecret", result);
+			print_error_exit(nameOfFunction, err_string(result));
+			Tspi_Context_Close(hContext);
+			exit(result);
+		}
+
+		// Call Change Auth
+		result = Tspi_ChangeAuth(hTPM, NULL_HKEY, hNewPolicy);
+		if (result == TSS_SUCCESS) {
+			print_success(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(0);
+		} else {
+			printf("Error resetting owner password.  Owner password"
+				" is now: \"%s\"\n", KEY_PWD);
+			if(!checkNonAPI(result)){
+				print_error(nameOfFunction, result);
+				print_end_test(nameOfFunction);
+				Tspi_Context_FreeMemory(hContext, NULL);
+				Tspi_Context_Close(hContext);
+				exit(1);
+			}
+			else{
+				print_error_nonapi(nameOfFunction, result);
+				print_end_test(nameOfFunction);
+				Tspi_Context_FreeMemory(hContext, NULL);
+				Tspi_Context_Close(hContext);
+				exit(1);
+			}
+		}
+	}
+}
