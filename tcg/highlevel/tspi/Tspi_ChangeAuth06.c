d846c10b019fd1e9480236b8dca6e20080168bf5
kyoder <kyoder>
2005-03-10 22:14:42 +0000

    initial add; tests change auth on the SRK

---
+++ tcg/highlevel/tspi/Tspi_ChangeAuth06.c
@@ -0,0 +1,242 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2004
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_ChangeAuth06
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_ChangeAuth works when changing the
+ *	authdata for the SRK itself.  The TSS spec admits openly that using
+ *	Tspi_ChangeAuth to change the auth for the SRK is a security risk,
+ *	so ChangeAuthAsym should be used, but this *should* work anyway.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect
+ *		Create hKeyChild
+ *		Load Key By UUID for hSRK
+ *		Get Policy Object for the srk
+ *		Set Secret
+ *		Create Storage Key
+ *		Create Signing Key
+ *		Load keys
+ *		Get Default Policy for the hPolicy
+ *
+ *	Test:	Call ChangeAuth. If it is not a success
+ *		Make sure that it returns the proper return codes
+ *
+ *	Cleanup:
+ *		Free memory associated with the context
+ *		Close hMSigningKey Object
+ *		Close hKeyChild Object
+ *		Close context
+ *		Print error/success message
+ *
+ * USAGE:	First parameter is --options
+ *			-v or --version
+ *		Second Parameter is the version of the test case to be run.
+ *		This test case is currently only implemented for 1.1
+ *
+ * HISTORY
+ *    Kent Yoder <shpedoikal@gmail.com>, March 10, 2005
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <tss/tss.h>
+#include "../common/common.h"
+
+extern TSS_UUID SRK_UUID;
+extern int commonErrors(TSS_RESULT result);
+
+#define NEW_PWD		"NEW PWD"
+
+int main(int argc, char **argv)
+{
+	char		*version;
+
+	version = parseArgs( argc, argv );
+		// if it is not version 1.1, print error
+	if(strcmp(version, "1.1")){
+		print_wrongVersion();
+	}
+	else{
+		main_v1_1();
+	}
+}
+
+main_v1_1(void){
+
+	char		*nameOfFunction = "Tspi_ChangeAuth06";
+	TSS_HKEY	hKeyChild, hKeyParent, hSRK;
+	TSS_HPOLICY	srkUsagePolicy, hTPMPolicy, hNewPolicy;
+	TSS_HCONTEXT	hContext;
+	TSS_RESULT	result;
+	TSS_HTPM	hTPM;
+
+	print_begin_test(nameOfFunction);
+
+		//Create Context
+	result = Tspi_Context_Create(&hContext);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Create ", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		exit(result);
+	}
+		//Connect Context
+	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Connect", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+
+	// Set up the SRK
+
+		//Load SRK By UUID
+	result = Tspi_Context_LoadKeyByUUID(hContext,
+			TSS_PS_TYPE_SYSTEM,
+			SRK_UUID, &hSRK);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_LoadKeyByUUID for hSRK", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+		//Get Policy Object
+	result = Tspi_GetPolicyObject(hSRK, TSS_POLICY_USAGE, &srkUsagePolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_GetPolicyObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+		//Set Secret
+	result = Tspi_Policy_SetSecret(srkUsagePolicy, TSS_SECRET_MODE_PLAIN,
+				0, NULL);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+
+	// get the TPM object
+	result = Tspi_Context_GetTpmObject(hContext, &hTPM);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_GetTpmObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+	result = Tspi_GetPolicyObject(hTPM, TSS_POLICY_USAGE, &hTPMPolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_GetPolicyObject", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Set Secret
+	result = Tspi_Policy_SetSecret(hTPMPolicy, TSS_SECRET_MODE_PLAIN,
+			strlen(OWN_PWD), OWN_PWD);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+
+		// create a new Policy
+	result = Tspi_Context_CreateObject(hContext, TSS_OBJECT_TYPE_POLICY,
+					TSS_POLICY_USAGE, &hNewPolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_GetPolicy ", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Set the new policy's Secret
+	result = Tspi_Policy_SetSecret(hNewPolicy, TSS_SECRET_MODE_PLAIN,
+			strlen(NEW_PWD), NEW_PWD);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(nameOfFunction, err_string(result));
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+
+		// Call Change Auth
+	result = Tspi_ChangeAuth(hSRK, hTPM, hNewPolicy);
+	if (result != TSS_SUCCESS) {
+		if(!checkNonAPI(result)){
+			print_error(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(1);
+		}
+		else{
+			print_error_nonapi(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(1);
+		}
+	}
+	else{
+		printf("Success.  Resetting the SRK password...\n");
+
+		// Call Change Auth, setting the TPM policy
+		result = Tspi_ChangeAuth(hSRK, hTPM, srkUsagePolicy);
+		if (result == TSS_SUCCESS) {
+			print_success(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_FreeMemory(hContext, NULL);
+			Tspi_Context_Close(hContext);
+			exit(0);
+		} else {
+			printf("Error resetting SRK password.  SRK password"
+				" is now: \"%s\"\n", NEW_PWD);
+			if(!checkNonAPI(result)){
+				print_error(nameOfFunction, result);
+				print_end_test(nameOfFunction);
+				Tspi_Context_FreeMemory(hContext, NULL);
+				Tspi_Context_Close(hContext);
+				exit(1);
+			}
+			else{
+				print_error_nonapi(nameOfFunction, result);
+				print_end_test(nameOfFunction);
+				Tspi_Context_FreeMemory(hContext, NULL);
+				Tspi_Context_Close(hContext);
+				exit(1);
+			}
+		}
+	}
+}
