f0187ec9add517365fdd244d9fcfc59645839e50
kyoder <kyoder>
2005-07-15 14:08:37 +0000

    simplified the testcase by removing unnecessary code

--- tcg/key/Tspi_Key_UnloadKey02.c
+++ tcg/key/Tspi_Key_UnloadKey02.c
@@ -103,99 +103,10 @@ main_v1_1( void )
 		exit( result );
 	}
 
-		// Connect to Context
-	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_Connect", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-		// create hKey
-/*	result = Tspi_Context_CreateObject( hContext,
-						TSS_OBJECT_TYPE_RSAKEY,
-						initFlags, &hKey );
-        if ( result != TSS_SUCCESS )
-        {
-                printf( "\tCreateObject failed. Unable to get hKey.\n" );
-                print_error_exit( function, err_string(result) );
-                exit( result );
-        }
-*/
-                //Load Key By UUID
-        result = Tspi_Context_LoadKeyByUUID( hContext, TSS_PS_TYPE_SYSTEM,
-						SRK_UUID, &hSRK );
-        if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_LoadKeyByUUID (hSRK)",
-				result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-                exit( result );
-	}
-
-                //Get Policy Object
-        result = Tspi_GetPolicyObject( hSRK, TSS_POLICY_USAGE, &srkUsagePolicy );
-        if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_GetPolicyObject", result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-        }
-
-                //Set Secret
-        result = Tspi_Policy_SetSecret( srkUsagePolicy, TSS_SECRET_MODE_PLAIN,
-				                0, NULL );
-        if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Policy_SetSecret", result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-                exit( result );
-        }
-
-		//Create Signing Key
-	result = Tspi_Context_CreateObject( hContext, TSS_OBJECT_TYPE_RSAKEY,
-						initFlags, &hKey );
-        if ( result != TSS_SUCCESS )
-        {
-                print_error( "Tspi_CreateObject (hKey)", result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-                exit( result );
-        }
-
-	result = Tspi_Key_CreateKey( hKey, hSRK, 0 );
-        if ( result != TSS_SUCCESS )
-        {
-                print_error( "Tspi_Key_CreateKey (hKey)", result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-                exit( result );
-        }
-
-	result = Tspi_Key_LoadKey( hKey, hSRK );
-	if ( result != TSS_SUCCESS )
-        {
-                print_error( "Tspi_Key_LoadKey (hKey)", result );
-                print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-                exit( result );
-        }
 
 		//Load Key Blob
-	result = Tspi_Key_UnloadKey( hKey );
-	if ( result != TSS_SUCCESS )
+	result = Tspi_Key_UnloadKey( 0xffffffff );
+	if ( TSS_ERROR_CODE(result) != TSS_E_INVALID_HANDLE )
 	{
 		if( !(checkNonAPI(result)) )
 		{
