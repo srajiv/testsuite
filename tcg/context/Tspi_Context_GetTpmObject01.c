bc6c9558bbd3756fa8fd57e7a834a5a1a7ec50ab
ratliff <ratliff>
2006-03-30 05:17:03 +0000

    The Tspi_Context_GetTpmObject API is unchanged between 1.1 and 1.2 so updated test cases to accept 1.2 as version parameter.

--- tcg/context/Tspi_Context_GetTpmObject01.c
+++ tcg/context/Tspi_Context_GetTpmObject01.c
@@ -1,6 +1,6 @@
 /*
  *
- *   Copyright (C) International Business Machines  Corp., 2004, 2005
+ *   Copyright (C) International Business Machines  Corp., 2004-2006
  *
  *   This program is free software;  you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -64,11 +64,11 @@ main( int argc, char **argv )
 	char		*version;
 
 	version = parseArgs( argc, argv );
-		// if it is not version 1.1, print error
-	if( strcmp(version, "1.1") )
-		print_wrongVersion();
-	else
+		// if it is not version 1.1 or 1.2, print error
+	if ((0 == strcmp(version, "1.1")) || (0 == strcmp(version, "1.2")))
 		main_v1_1();
+	else
+		print_wrongVersion();
 }
 
 int
