a121a4fa66c63d358b6d34e21958b8db3921a891
kyoder <kyoder>
2006-06-02 20:35:20 +0000

    removed unneeded code

--- tcg/context/Tspi_Context_UnregisterKey03.c
+++ tcg/context/Tspi_Context_UnregisterKey03.c
@@ -23,9 +23,9 @@
  *
  * DESCRIPTION
  *	This test will verify Tspi_Context_UnregisterKey
- *	The purpose of this test case is to get TSS_E_BAD_PARAMETER to 
- *		be returned. This is accomplished by
- *		not initializing the MigratableSigningUUID. 
+ *	The purpose of this test case is to get TSS_E_BAD_PARAMETER to
+ *		be returned. This is accomplished by passing in a NULL value
+ *		to receive a key handle.
  *
  * ALGORITHM
  *	Setup:
@@ -42,9 +42,9 @@
  *		SetAttribUint32 (x2)
  *		RegisterKey
  *
- *	Test:	Call UnregisterKey. If this is unsuccessful check for 
+ *	Test:	Call UnregisterKey. If this is unsuccessful check for
  *		type of error, and make sure it returns the proper return code
- * 
+ *
  *	Cleanup:
  *		Free memory associated with the context
  *		Close the hKey object
@@ -93,12 +93,11 @@ main_v1_1(void){
 	TSS_HKEY	hSRK;
 	TSS_RESULT	result;
 	TSS_UUID	migratableSignUUID;
-	TSS_HPOLICY	srkUsagePolicy, keyUsagePolicy, keyMigPolicy;
-	initFlags	= TSS_KEY_TYPE_SIGNING | TSS_KEY_SIZE_2048  |
+	TSS_HPOLICY	srkUsagePolicy;
+	initFlags	= TSS_KEY_TYPE_SIGNING | TSS_KEY_SIZE_512  |
 			TSS_KEY_VOLATILE | TSS_KEY_NO_AUTHORIZATION |
 			TSS_KEY_NOT_MIGRATABLE;
-	TSS_FLAG	badPSType = TSS_PS_TYPE_SYSTEM + TSS_PS_TYPE_USER;
-	BYTE		well_known_secret[20] = TSS_WELL_KNOWN_SECRET;
+	TSS_FLAG	badPSType = 0xffffffff;
 
 	print_begin_test(nameOfFunction);
 
@@ -112,13 +111,6 @@ main_v1_1(void){
 	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
 	if (result != TSS_SUCCESS) {
 		print_error("Tspi_Context_Connect ", result);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		//Get TPM Object
-	result = Tspi_Context_GetTpmObject(hContext, &hTPM);
-	if (result != TSS_SUCCESS) {
-		print_error("Tspi_Context_GetTpmObject ", result);
 		Tspi_Context_Close(hContext);
 		exit(result);
 	}
@@ -157,42 +149,6 @@ main_v1_1(void){
 		Tspi_Context_Close(hContext);
 		exit(result);
 	}
-		//Get Policy Object for the keyUsagePolicy
-	result = Tspi_GetPolicyObject(hKey, TSS_POLICY_USAGE, 
-					&keyUsagePolicy);
-	if (result != TSS_SUCCESS) {
-		print_error("Tspi_GetPolicyObject ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		//Get Policy Object for the keyMigPolicy
-	result = Tspi_GetPolicyObject(hKey, TSS_POLICY_MIGRATION,
-					&keyMigPolicy);
-	if (result != TSS_SUCCESS) {
-		print_error("Tspi_GetPolicyObject ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		//Set Secret 
-	result = Tspi_Policy_SetSecret(keyMigPolicy, TSS_SECRET_MODE_SHA1,
-					20, well_known_secret);
-	if (result != TSS_SUCCESS) {
-		print_error("Tspi_Policy_SetSecret ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		//Set Secret
-	result = Tspi_Policy_SetSecret(keyUsagePolicy, TSS_SECRET_MODE_SHA1, 
-					20, well_known_secret);
-	if (result != TSS_SUCCESS) {
-		print_error("Tspi_Policy_SetSecret ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
 		//Create the hKey with the hSRK wrapping key
 	result = Tspi_Key_CreateKey(hKey, hSRK, 0);
 	if (result != TSS_SUCCESS) {
@@ -201,27 +157,6 @@ main_v1_1(void){
 		Tspi_Context_Close(hContext);
 		exit(result);
 	}
-		//SetAttribUint32
-	result = Tspi_SetAttribUint32(hKey, TSS_TSPATTRIB_KEY_INFO,
-				TSS_TSPATTRIB_KEYINFO_ENCSCHEME,
-				TSS_ES_NONE);
-	if (result != TSS_SUCCESS) { 
-		print_error("Tspi_SetAttribUint32 ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		//SetAttribUint32
-	result = Tspi_SetAttribUint32(hKey, TSS_TSPATTRIB_KEY_INFO,
-				TSS_TSPATTRIB_KEYINFO_SIGSCHEME,
-				TSS_ES_NONE);
-	if (result != TSS_SUCCESS) { 
-		print_error("Tspi_SetAttribUint32 ", result);
-		Tspi_Context_CloseObject(hContext, hKey);
-		Tspi_Context_Close(hContext);
-		exit(result);
-	}
-		
 		//Register the hKey
 	result = Tspi_Context_RegisterKey(hContext,
 				hKey, TSS_PS_TYPE_USER, migratableSignUUID,
@@ -233,9 +168,8 @@ main_v1_1(void){
 		exit(result);
 	}
 		//UnregisterKey
-	result = Tspi_Context_UnregisterKey(hContext, 
-			badPSType, migratableSignUUID,
-			&hKey);
+	result = Tspi_Context_UnregisterKey(hContext, TSS_PS_TYPE_USER, migratableSignUUID,
+					    NULL);
 	if (TSS_ERROR_CODE(result) != TSS_E_BAD_PARAMETER) {	
 		if(!checkNonAPI(result)){
 			print_error(nameOfFunction, result);
