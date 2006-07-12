6081cfe817f5813994aebb1b14ce39875b01228e
kyoder <kyoder>
2006-07-12 16:19:24 +0000

    initial add - tests read/write of a dir register

---
+++ tcg/highlevel/tpm/Tspi_TPM_DirWrite04.c
@@ -0,0 +1,182 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2004, 2005
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_TPM_DirWrite04.c
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_TPM_DirWrite writes the correct
+ *	value to the DIR register by reading the written value back out.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect Context
+ *		Get TPM Object
+ *		Get Policy Object
+ *		Set Secret
+ *
+ *	Test:
+ *		Call TPM_DirWrite then if it does not succeed
+ *		Make sure that it returns the proper return codes
+ *		Print results
+ *
+ *	Cleanup:
+ *		Free memory relating to hContext
+ *		Close context
+ *
+ * USAGE
+ *      First parameter is --options
+ *                         -v or --version
+ *      Second parameter is the version of the test case to be run
+ *      This test case is currently only implemented for v1.1
+ *
+ * HISTORY
+ *	Kent Yoder, kyoder@users.sf.net, 7/06
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <stdio.h>
+#include <trousers/tss.h>
+#include "../common/common.h"
+
+int
+main( int argc, char **argv )
+{
+	char			*version;
+
+	version = parseArgs( argc, argv );
+		// if it is not version 1.1, print error
+	if( strcmp(version, "1.1") )
+		print_wrongVersion();
+	else
+		main_v1_1();
+}
+
+int
+main_v1_1( void )
+{
+	char			*function = "Tspi_TPM_DirWrite04";
+	BYTE			rgbDirData[20], *prgbDirData;
+	TSS_HCONTEXT		hContext;
+	TSS_HPOLICY		hPolicy;
+	TSS_HTPM		hTPM;
+	TSS_RESULT		result;
+	UINT32			pulDirDataLength;
+
+	print_begin_test( function );
+
+		// Create Context
+	result = Tspi_Context_Create( &hContext );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Create", result );
+		print_error_exit( function, err_string(result) );
+		exit( result );
+	}
+
+		// Connect to Context
+	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Connect", result );
+		print_error_exit( function, err_string(result) );
+		exit( result );
+	}
+
+		// Retrieve TPM object of context
+	result = Tspi_Context_GetTpmObject( hContext, &hTPM );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_GetTpmObject", result );
+		print_error_exit( function, err_string(result) );
+		exit( result );
+	}
+
+	result = Tspi_GetPolicyObject( hTPM, TSS_POLICY_USAGE, &hPolicy );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_GetPolicyObject", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	result = Tspi_Policy_SetSecret( hPolicy, TESTSUITE_OWNER_SECRET_MODE,
+					TESTSUITE_OWNER_SECRET_LEN, TESTSUITE_OWNER_SECRET );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Policy_SetSecret", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	memset( rgbDirData, 0x06, 20 );
+
+		//Get random number
+	result = Tspi_TPM_DirWrite( hTPM, 0, 20, rgbDirData );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_TPM_DirWrite", result );
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	result = Tspi_TPM_DirRead( hTPM, 0, &pulDirDataLength, &prgbDirData );
+	if ( result != TSS_SUCCESS )
+	{
+		if( !(checkNonAPI(result)) )
+		{
+			print_error( function, result );
+		}
+		else
+		{
+			print_error_nonapi( function, result );
+		}
+	}
+	else
+	{
+		if (pulDirDataLength == 20) {
+			if (!memcmp(rgbDirData, prgbDirData, pulDirDataLength)) {
+				print_success( function, TSS_SUCCESS );
+			} else {
+				fprintf(stderr, "FAIL: Read DIR register contents don't match!\n");
+				print_error( function, result );
+				result = 255;
+			}
+		} else {
+			fprintf(stderr, "FAIL: Size of DIR register contents don't match!\n");
+			print_error( function, result );
+			result = 255;
+		}
+	}
+
+	print_end_test( function );
+	Tspi_Context_FreeMemory( hContext, NULL );
+	Tspi_Context_Close( hContext );
+	exit( result );
+}
