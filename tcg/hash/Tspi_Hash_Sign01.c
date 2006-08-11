60003d3c475ebda6da8175aaf7cd480478faeb03
ratliff <ratliff>
2006-08-11 03:22:48 +0000

    formatting & 1.2

--- tcg/hash/Tspi_Hash_Sign01.c
+++ tcg/hash/Tspi_Hash_Sign01.c
@@ -1,6 +1,6 @@
 /*
  *
- *   Copyright (C) International Business Machines  Corp., 2004, 2005
+ *   Copyright (C) International Business Machines  Corp., 2004-2006
  *
  *   This program is free software;  you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -52,10 +52,11 @@
  *      First parameter is --options
  *                         -v or --version
  *      Second parameter is the version of the test case to be run
- *      This test case is currently only implemented for v1.1
+ *      This test case is currently only implemented for v1.1 and 1.2
  *
  * HISTORY
  *      Megan Schneider, mschnei@us.ibm.com, 6/04.
+ *	EJR, ejratl@gmail.com, 8/10/2006, 1.2 updates
  *
  * RESTRICTIONS
  *	None.
@@ -66,180 +67,159 @@
 #include "../common/common.h"
 
 
-int
-main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	char		*version;
+	char *version;
 
-	version = parseArgs( argc, argv );
-		// if it is not version 1.1, print error
-	if( strcmp(version, "1.1") )
-		print_wrongVersion();
-	else
+	version = parseArgs(argc, argv);
+	// if it is not version 1.1 or 1.2, print error
+	if ((0 == strcmp(version, "1.1")) || (0 == strcmp(version, "1.2")))
 		main_v1_1();
+	else
+		print_wrongVersion();
 }
 
-int
-main_v1_1( void )
+int main_v1_1(void)
 {
-	char		*function = "Tspi_Hash_Sign01";
-	TSS_HCONTEXT	hContext;
-	TSS_HKEY	hSRK;
-	TSS_HKEY	hMSigningKey;
-	TSS_UUID	SRKUUID			= {0,0,0,0,0,0,0,0,0,0,1};
-	TSS_UUID	migratableSignUUID	= {1,2,3,4,5,6,7,8,9,10,2};
-	TSS_HHASH	hHash;
-	BYTE		*prgbSignature;
-	UINT32		pulSignatureLength;
-	TSS_RESULT	result;
-	TSS_HPOLICY	srkUsagePolicy;
+	char *function = "Tspi_Hash_Sign01";
+	TSS_HCONTEXT hContext;
+	TSS_HKEY hSRK;
+	TSS_HKEY hMSigningKey;
+	TSS_UUID SRKUUID = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+	TSS_UUID migratableSignUUID = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2 };
+	TSS_HHASH hHash;
+	BYTE *prgbSignature;
+	UINT32 pulSignatureLength;
+	TSS_RESULT result;
+	TSS_HPOLICY srkUsagePolicy;
 
-	print_begin_test( function );
+	print_begin_test(function);
 
-		// Create Context
-	result = Tspi_Context_Create( &hContext );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_Create", result );
-		print_error_exit( function, err_string(result) );
-		exit( result );
+	// Create Context
+	result = Tspi_Context_Create(&hContext);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Create", result);
+		print_error_exit(function, err_string(result));
+		exit(result);
+	}
+	// Connect to Context
+	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Connect", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+	//Load Key By UUID
+	result = Tspi_Context_LoadKeyByUUID(hContext, TSS_PS_TYPE_SYSTEM,
+					    SRK_UUID, &hSRK);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_LoadKeyByUUID (hSRK)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+	//Get Policy Object
+	result = Tspi_GetPolicyObject(hSRK, TSS_POLICY_USAGE,
+				      &srkUsagePolicy);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_GetPolicyObject", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+	//Set Secret
+	result =
+	    Tspi_Policy_SetSecret(srkUsagePolicy,
+				  TESTSUITE_SRK_SECRET_MODE,
+				  TESTSUITE_SRK_SECRET_LEN,
+				  TESTSUITE_SRK_SECRET);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Policy_SetSecret", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+	//Create Signing Key
+	result =
+	    Tspi_Context_CreateObject(hContext, TSS_OBJECT_TYPE_RSAKEY,
+				      TSS_KEY_SIZE_2048 |
+				      TSS_KEY_TYPE_SIGNING |
+				      TSS_KEY_MIGRATABLE |
+				      TSS_KEY_NO_AUTHORIZATION,
+				      &hMSigningKey);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_CreateObject (signing key)",
+			    result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-		// Connect to Context
-	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_Connect", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
+	result = Tspi_Key_CreateKey(hMSigningKey, hSRK, 0);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Key_CreateKey (signing key)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-		//Load Key By UUID
-	result = Tspi_Context_LoadKeyByUUID( hContext, TSS_PS_TYPE_SYSTEM,
-						SRK_UUID, &hSRK );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_LoadKeyByUUID (hSRK)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
+	result = Tspi_Key_LoadKey(hMSigningKey, hSRK);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_LoadKey (hMSigningKey)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
+	}
+	// create hash
+	result = Tspi_Context_CreateObject(hContext, TSS_OBJECT_TYPE_HASH,
+					   TSS_HASH_SHA1, &hHash);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_CreateObject (hash)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_UnregisterKey(hContext, TSS_PS_TYPE_SYSTEM,
+					   migratableSignUUID,
+					   &hMSigningKey);
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-		//Get Policy Object
-	result = Tspi_GetPolicyObject( hSRK, TSS_POLICY_USAGE,
-					&srkUsagePolicy );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_GetPolicyObject", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
+	result = Tspi_Hash_SetHashValue(hHash, 20, "Je pense, danc je s");
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Hash_SetHashValue", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_UnregisterKey(hContext, TSS_PS_TYPE_SYSTEM,
+					   migratableSignUUID,
+					   &hMSigningKey);
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-		//Set Secret
-	result = Tspi_Policy_SetSecret( srkUsagePolicy, TESTSUITE_SRK_SECRET_MODE,
-				TESTSUITE_SRK_SECRET_LEN, TESTSUITE_SRK_SECRET );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Policy_SetSecret", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-		//Create Signing Key
-	result = Tspi_Context_CreateObject( hContext, TSS_OBJECT_TYPE_RSAKEY,
-						TSS_KEY_SIZE_2048 |
-						TSS_KEY_TYPE_SIGNING |
-						TSS_KEY_MIGRATABLE |
-						TSS_KEY_NO_AUTHORIZATION,
-						&hMSigningKey );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_CreateObject (signing key)",
-				result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-	result = Tspi_Key_CreateKey( hMSigningKey, hSRK, 0 );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Key_CreateKey (signing key)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-	result = Tspi_Key_LoadKey( hMSigningKey, hSRK );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_LoadKey (hMSigningKey)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-		// create hash
-	result = Tspi_Context_CreateObject( hContext, TSS_OBJECT_TYPE_HASH,
-						TSS_HASH_SHA1, &hHash );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_CreateObject (hash)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_UnregisterKey( hContext, TSS_PS_TYPE_SYSTEM,
-						migratableSignUUID,
-						&hMSigningKey );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-	result = Tspi_Hash_SetHashValue( hHash, 20, "Je pense, danc je s" );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Hash_SetHashValue", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_UnregisterKey( hContext, TSS_PS_TYPE_SYSTEM,
-						migratableSignUUID,
-						&hMSigningKey );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-	result = Tspi_Hash_Sign( hHash, hMSigningKey, &pulSignatureLength,
-					&prgbSignature );
-	if ( result != TSS_SUCCESS )
-	{
-		if( !(checkNonAPI(result)) )
-		{
-			print_error( function, result );
+	result = Tspi_Hash_Sign(hHash, hMSigningKey, &pulSignatureLength,
+				&prgbSignature);
+	if (result != TSS_SUCCESS) {
+		if (!(checkNonAPI(result))) {
+			print_error(function, result);
+		} else {
+			print_error_nonapi(function, result);
 		}
-		else
-		{
-			print_error_nonapi( function, result );
-		}
-	}
-	else
-	{
-		print_success( function, result );
+	} else {
+		print_success(function, result);
 	}
 
-	print_end_test( function );
-	Tspi_Context_UnregisterKey( hContext, TSS_PS_TYPE_SYSTEM,
-					migratableSignUUID,
-					&hMSigningKey );
-	Tspi_Context_FreeMemory( hContext, NULL );
-	Tspi_Context_Close( hContext );
-	exit( result );
+	print_end_test(function);
+	Tspi_Context_UnregisterKey(hContext, TSS_PS_TYPE_SYSTEM,
+				   migratableSignUUID, &hMSigningKey);
+	Tspi_Context_FreeMemory(hContext, NULL);
+	Tspi_Context_Close(hContext);
+	exit(result);
 }
