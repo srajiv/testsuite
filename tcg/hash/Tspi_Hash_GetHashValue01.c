fd1e13a153c261a2f4ee7d7df19225068da901c4
ratliff <ratliff>
2006-08-11 03:07:31 +0000

    formatting & 1.2

--- tcg/hash/Tspi_Hash_GetHashValue01.c
+++ tcg/hash/Tspi_Hash_GetHashValue01.c
@@ -1,6 +1,6 @@
 /*
  *
- *   Copyright (C) International Business Machines  Corp., 2004, 2005
+ *   Copyright (C) International Business Machines  Corp., 2004-2006
  *
  *   This program is free software;  you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -44,10 +44,11 @@
  *      First parameter is --options
  *                         -v or --version
  *      Second parameter is the version of the test case to be run
- *      This test case is currently only implemented for v1.1
+ *      This test case is currently only implemented for v1.1 and 1.2
  *
  * HISTORY
  *      Megan Schneider, mschnei@us.ibm.com, 6/04.
+ *	EJR, ejratl@gmail.com, 8/10/2006, 1.2
  *
  * RESTRICTIONS
  *	None.
@@ -58,87 +59,74 @@
 #include "../common/common.h"
 
 
-int
-main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-	char		*version;
+	char *version;
 
-	version = parseArgs( argc, argv );
-		// if it is not version 1.1, print error
-	if( strcmp(version, "1.1") )
-		print_wrongVersion();
-	else
+	version = parseArgs(argc, argv);
+	// if it is not version 1.1 or 1.2, print error
+	if ((0 == strcmp(version, "1.1")) || (0 == strcmp(version, "1.2")))
 		main_v1_1();
+	else
+		print_wrongVersion();
 }
 
-int
-main_v1_1( void )
+int main_v1_1(void)
 {
-	char		*function = "Tspi_Hash_GetHashValue01";
-	TSS_HCONTEXT	hContext;
-	TSS_HHASH	hHash;
-	UINT32		ulHashValueLength;
-	BYTE		*rgbHashValue, hashBuf[20];
-	TSS_RESULT	result;
+	char *function = "Tspi_Hash_GetHashValue01";
+	TSS_HCONTEXT hContext;
+	TSS_HHASH hHash;
+	UINT32 ulHashValueLength;
+	BYTE *rgbHashValue, hashBuf[20];
+	TSS_RESULT result;
 
-	print_begin_test( function );
+	print_begin_test(function);
 
-		// Create Context
-	result = Tspi_Context_Create( &hContext );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_Create", result );
-		print_error_exit( function, err_string(result) );
-		exit( result );
+	// Create Context
+	result = Tspi_Context_Create(&hContext);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Create", result);
+		print_error_exit(function, err_string(result));
+		exit(result);
+	}
+	// Connect to Context
+	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Connect", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-		// Connect to Context
-	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_Connect", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
+	result = Tspi_Context_CreateObject(hContext, TSS_OBJECT_TYPE_HASH,
+					   TSS_HASH_SHA1, &hHash);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_CreateObject (hash)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-	result = Tspi_Context_CreateObject( hContext, TSS_OBJECT_TYPE_HASH,
-						TSS_HASH_SHA1, &hHash );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_CreateObject (hash)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
+	result = Tspi_Hash_SetHashValue(hHash, 20, hashBuf);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_CreateObject (hash)", result);
+		print_error_exit(function, err_string(result));
+		Tspi_Context_FreeMemory(hContext, NULL);
+		Tspi_Context_Close(hContext);
+		exit(result);
 	}
 
-	result = Tspi_Hash_SetHashValue( hHash, 20, hashBuf );
-	if ( result != TSS_SUCCESS )
-	{
-		print_error( "Tspi_Context_CreateObject (hash)", result );
-		print_error_exit( function, err_string(result) );
-		Tspi_Context_FreeMemory( hContext, NULL );
-		Tspi_Context_Close( hContext );
-		exit( result );
-	}
-
-	result = Tspi_Hash_GetHashValue( hHash, &ulHashValueLength,
-						&rgbHashValue );
-	if ( result != TSS_SUCCESS )
-	{
-		if( !(checkNonAPI(result)) )
-		{
-			print_error( function, result );
+	result = Tspi_Hash_GetHashValue(hHash, &ulHashValueLength,
+					&rgbHashValue);
+	if (result != TSS_SUCCESS) {
+		if (!(checkNonAPI(result))) {
+			print_error(function, result);
+		} else {
+			print_error_nonapi(function, result);
 		}
-		else
-		{
-			print_error_nonapi( function, result );
-		}
-	}
-	else
-	{
+	} else {
 		result = Tspi_Context_FreeMemory(hContext, rgbHashValue);
 		if (result != TSS_SUCCESS) {
 			print_error("Tspi_Context_FreeMemory ", result);
@@ -146,11 +134,11 @@ main_v1_1( void )
 			Tspi_Context_Close(hContext);
 			exit(result);
 		}
-		print_success( function, result );
+		print_success(function, result);
 	}
 
-	print_end_test( function );
-	Tspi_Context_FreeMemory( hContext, NULL );
-	Tspi_Context_Close( hContext );
-	exit( result );
+	print_end_test(function);
+	Tspi_Context_FreeMemory(hContext, NULL);
+	Tspi_Context_Close(hContext);
+	exit(result);
 }
