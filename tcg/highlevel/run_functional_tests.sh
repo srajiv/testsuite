f3e935af2436b39efa6b82a5a50969fc7fd593c1
kyoder <kyoder>
2006-09-01 16:52:49 +0000

    added more tests; added a success message

--- tcg/highlevel/run_functional_tests.sh
+++ tcg/highlevel/run_functional_tests.sh
@@ -14,19 +14,28 @@ else
 	VERSION=$1
 fi
 
+if test x`which rngtest` != x; then
+	./tpm_rng_test | rngtest
+fi
 
+# context tests
 ./context/Tspi_Context_Connect03 -v $VERSION || exit $?
 ./context/Tspi_Context_LoadKeyByBlob04 -v $VERSION || exit $?
 ./context/Tspi_Context_RegisterKey05 -v $VERSION || exit $?
 ./context/Tspi_Context_LoadKeyByUUID06 -v $VERSION || exit $?
 ./context/Tspi_Context_Connect04 -v $VERSION || exit $?
+./context/Tspi_Context_CreateDataObject01 -v $VERSION || exit $?
+./context/Tspi_Context_CreateHashObject01 -v $VERSION || exit $?
+./context/Tspi_Context_CreatePcrsObject01 -v $VERSION || exit $?
+./context/Tspi_Context_CreatePolicyObject01 -v $VERSION || exit $?
 
-./key/create_key_tests.sh -v $VERSION || exit $?
+# key tests
 ./key/Tspi_Key_CreateKey05 -v $VERSION || exit $?
 # Warning: These two will trash a PCR!
 ./key/Tspi_Key_CreateKeyWithPcrs -v $VERSION || exit $?
 ./key/Tspi_Key_WrapKeyToPcr -v $VERSION || exit $?
 
+# tpm tests
 ./tpm/Tspi_TPM_Quote05 -v $VERSION || exit $?
 ./tpm/Tspi_TPM_PcrRead04 -v $VERSION || exit $?
 # Warning: Tspi_TPM_PcrExtend05 will trash all PCRs
@@ -42,6 +51,7 @@ fi
 ./tpm/Tspi_TPM_GetCapability07 -v $VERSION || exit $?
 ./tpm/Tspi_TPM_DirWrite04 -v $VERSION || exit $?
 
+# tspi tests
 ./tspi/Tspi_Callbacks01 -v $VERSION || exit $?
 ./tspi/Tspi_ChangeAuth05 -v $VERSION || exit $?
 ./tspi/Tspi_ChangeAuth06 -v $VERSION || exit $?
@@ -49,6 +59,7 @@ fi
 ./tspi/Tspi_Context_GetCapability18 -v $VERSION || exit $?
 ./tspi/Tspi_Context_GetCapability19 -v $VERSION || exit $?
 
+# data tests
 ./data/Tspi_Data_Unbind05 -v $VERSION || exit $?
 ./data/Tspi_Data_Unbind06 -v $VERSION || exit $?
 ./data/Tspi_Data_Unbind07 -v $VERSION || exit $?
@@ -59,5 +70,8 @@ fi
 # XXX This is an incomplete list...
 
 
+echo "************************"
+echo "$0: ALL TESTS SUCCEEDED "
+echo "************************"
 
 exit 0
