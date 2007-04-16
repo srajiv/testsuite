297eed78de6d8f8e51025fd4267fe85362a3f9d1
kyoder <kyoder>
2007-04-16 15:54:05 +0000

    added support for running against a 1.2 TSS

--- tcg/tpm/Tspi_TPM_GetPubEndorsementKey01.c
+++ tcg/tpm/Tspi_TPM_GetPubEndorsementKey01.c
@@ -60,10 +60,85 @@ int main(int argc, char **argv)
 	char version;
 
 	version = parseArgs( argc, argv );
-	if (version)
+	if (version == TESTSUITE_TEST_TSS_1_2)
+		main_v1_2(version);
+	else if (version)
 		main_v1_1();
 	else
 		print_wrongVersion();
+}
+
+main_v1_2(char version){
+
+	char		*nameOfFunction = "Tspi_TPM_GetPubEndorsementKey01";
+	TSS_RESULT	result;
+	TSS_HCONTEXT	hContext;
+	TSS_HTPM	hTPM;
+	TSS_HKEY	hSRK, hEndorsement;
+	TSS_HPOLICY	hPolicy;
+
+	print_begin_test(nameOfFunction);
+
+		//Create Context
+	result = Tspi_Context_Create(&hContext);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Create", result);
+		exit(result);
+	}
+		//Connect Context
+	result = Tspi_Context_Connect(hContext, get_server(GLOBALSERVER));
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_Connect", result);
+		exit(result);
+	}
+		//Get TPM Object
+	result = Tspi_Context_GetTpmObject(hContext, &hTPM);
+	if (result != TSS_SUCCESS) {
+		print_error("Tspi_Context_GetTpmObject", result);
+		exit(result);
+	}
+
+	result = Tspi_GetPolicyObject( hTPM, TSS_POLICY_USAGE, &hPolicy );
+	if ( result != TSS_SUCCESS ) {
+		print_error( "Tspi_GetPolicyObject", result );
+		return result;
+	}
+
+	result = Tspi_Policy_SetSecret( hPolicy, TESTSUITE_OWNER_SECRET_MODE,
+					TESTSUITE_OWNER_SECRET_LEN, TESTSUITE_OWNER_SECRET);
+	if ( result != TSS_SUCCESS ) {
+		print_error( "Tspi_Policy_SetSecret", result );
+		return result;
+	}
+
+	result = Tspi_TPM_SetStatus( hTPM, TSS_TPMSTATUS_DISABLEPUBEKREAD, FALSE );
+	if ( result != TSS_SUCCESS ) {
+		print_error( "Tspi_TPM_SetStatus", result );
+		return result;
+	}
+
+		//Get Public EndorsementKey
+	result = Tspi_TPM_GetPubEndorsementKey(hTPM, TRUE, NULL, &hEndorsement);
+	if (result != TSS_SUCCESS) {
+		if(!checkNonAPI(result)){
+			print_error(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_Close(hContext);
+			exit(result);
+		}
+		else{
+			print_error_nonapi(nameOfFunction, result);
+			print_end_test(nameOfFunction);
+			Tspi_Context_Close(hContext);
+			exit(result);
+		}
+	}
+	else{
+		print_success(nameOfFunction, result);
+		print_end_test(nameOfFunction);
+		Tspi_Context_Close(hContext);
+		exit(0);
+	}
 }
 
 main_v1_1(void){
