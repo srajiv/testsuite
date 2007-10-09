b96bac86f486c738ff2eff93e671a3aa3b483d23
kyoder <kyoder>
2007-10-09 22:08:45 +0000

    initial add

---
+++ tcg/tpm/Tspi_TPM_Delegate_AddFamily04.c
@@ -0,0 +1,129 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2007
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_TPM_Delegate_AddFamily04.c
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_TPM_Delegate_AddFamily returns a bad parameter error 
+ *		when needed.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect Context
+ *		Get TPM Handle
+ *
+ *	Test:
+ *		Call TPM_Delegate_AddFamily.
+ *		Make sure that it returns the proper return codes
+ *		Print results
+ *
+ *	Cleanup:
+ *		Free memory related to hContext
+ *		Close context
+ *
+ * USAGE
+ *      First parameter is --options
+ *                         -v or --version
+ *      Second parameter is the version of the test case to be run
+ *      This test case is currently only implemented for v1.2
+ *
+ * HISTORY
+ *      Giampaolo Libralao, glibrala@br.ibm.com - 09/2007.
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <stdio.h>
+#include "common.h"
+
+
+int
+main( int argc, char **argv )
+{
+	char		version;
+
+	version = parseArgs( argc, argv );
+	if (version >= TESTSUITE_TEST_TSS_1_2)
+		main_v1_2(version);
+	else
+		print_NA();
+}
+
+int
+main_v1_2( char version )
+{
+	char *			function = "Tspi_TPM_Delegate_AddFamily04";
+	TSS_HCONTEXT		hContext;
+	TSS_HKEY		hSRK;
+	TSS_HTPM		hTPM;
+	TSS_HPOLICY		hTPMPolicy;
+	TSS_RESULT		result;
+
+	print_begin_test( function );
+
+	result = connect_load_all(&hContext, &hSRK, &hTPM);
+	if ( result != TSS_SUCCESS )
+	{
+		print_error_exit( function, err_string(result) );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	result = Tspi_GetPolicyObject( hTPM, TSS_POLICY_USAGE, &hTPMPolicy );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_GetPolicyObject", result );
+		print_error_exit( function, err_string(result) );
+		goto done;
+	}
+
+	result = Tspi_Policy_SetSecret( hTPMPolicy, TESTSUITE_OWNER_SECRET_MODE,
+					TESTSUITE_OWNER_SECRET_LEN, TESTSUITE_OWNER_SECRET );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Policy_SetSecret", result );
+		print_error_exit( function, err_string(result) );
+		goto done;
+	}
+
+	result = Tspi_TPM_Delegate_AddFamily(hTPM, 'a', NULL);
+	if (TSS_ERROR_CODE(result) != TSS_E_BAD_PARAMETER)
+	{
+		print_error_exit( function, err_string(result) );
+		result = 1;
+		goto done;
+	}
+	else
+	{
+		print_success( function, result );
+		result = 0;
+	}
+
+done:
+	Tspi_Context_FreeMemory( hContext, NULL );
+	Tspi_Context_Close( hContext );
+
+	print_end_test( function );
+	exit( result );
+}
---
+++ tcg/tpm/Tspi_TPM_RevokeEndorsementKey04.c
@@ -0,0 +1,138 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2007
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_TPM_RevokeEndorsementKey04.c
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_TPM_RevokeEndorsementKey
+ *		returns TSS_SUCCESS.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect Context
+ *		Get TPM Object
+ *		Create Object
+ *		Set Attrib Uint (twice)
+ *		Create a revocable endorsement key
+ *
+ *	Test:
+ *		Call TPM_RevokeEndorsementKey then if it does not succeed
+ *		Make sure that it returns the proper return codes
+ *		Print results
+ *
+ *	Cleanup:
+ *		Free memory relating to hContext
+ *		Close context
+ *
+ * USAGE
+ *      First parameter is --options
+ *                         -v or --version
+ *      Second parameter is the version of the test case to be run
+ *      This test case is currently only implemented for v1.1
+ *
+ * HISTORY
+ *      Tom Lendacky, toml@us.ibm.com, 8/07.
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <stdio.h>
+#include "common.h"
+
+int
+main( int argc, char **argv )
+{
+	char version;
+
+	version = parseArgs( argc, argv );
+	if (version >= TESTSUITE_TEST_TSS_1_2)
+		main_v1_2(version);
+	else
+		print_NA();
+}
+
+int
+main_v1_2( char version )
+{
+	char			*function = "Tspi_TPM_RevokeEndorsementKey04";
+	TSS_HCONTEXT		hContext;
+	TSS_HTPM		hTPM;
+	TSS_RESULT		result;
+	BYTE			resetData[TPM_SHA1BASED_NONCE_LEN];
+	UINT32			resetAuthSize = sizeof(resetData);
+	BYTE			*resetAuth = resetData;
+	UINT32			exitCode = 0;
+
+	print_begin_test( function );
+
+		// Create Context
+	result = Tspi_Context_Create( &hContext );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Create", result );
+		exit( result );
+	}
+
+		// Connect to Context
+	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Connect", result );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+		// Retrieve TPM object of context
+	result = Tspi_Context_GetTpmObject( hContext, &hTPM );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_GetTpmObject", result );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	result = Tspi_TPM_RevokeEndorsementKey( hTPM, resetAuthSize, NULL );
+	if ( TSS_ERROR_CODE(result) != TSS_E_BAD_PARAMETER )
+	{
+		if( !(checkNonAPI(result)) )
+		{
+			print_error( function, result );
+		}
+		else
+		{
+			print_error_nonapi( function, result );
+		}
+		exitCode = result;
+	}
+	else
+	{
+		print_success( function, result );
+	}
+
+	print_end_test( function );
+	Tspi_Context_FreeMemory( hContext, NULL );
+	Tspi_Context_Close( hContext );
+	exit( exitCode );
+}
