3763ce6616ec7be81db7acc94ad4e486278497e7
kyoder <kyoder>
2007-10-09 22:11:54 +0000

    added formatting option for output, fixed bugs

--- tsstests.sh
+++ tsstests.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-##
+#!/bin/bash
+#
 #
 #   Copyright (C) International Business Machines  Corp., 2004
 #
@@ -29,13 +29,11 @@
 #      None.
 #
 # USAGE
-#      ./tsstests.sh [-v version] [-l logfile]
-#	-v  version of the tss spec to use
-#	-l  logfile to redirect output to
 #
 # HISTORY
 #      Megan Schneider, mschnei@us.ibm.com, 6/04.
 #      kyoder@users.sf.net, Added shifts to the option processing
+#                           Added output format options
 #
 # RESTRICTIONS
 #      None.
@@ -46,15 +44,14 @@ export TSS_VERSION=1.1
 LOGDIR=$PWD
 export LOGFILE=$LOGDIR/tsstests.log
 LOGGING=0
-PASSFAIL=0
-PASSED=0
-FAILED=0
-SEGFAULTED=0
-NOTIMPL=0
-NA=0
-PRETENDING=0
+INIT=0
 QUIET=0
 export ERR_SUMMARY=../../err.summary
+SPECIFIC_TEST_DIR=
+
+TEST_RUN=
+TEST_OUTPUT=
+OUTPUT_FORMAT="standard"
 
 # this variable needs to be changed to testcases/tcg/ for ltp compatibility
 TESTCASEDIR=testsuite/tcg/
@@ -68,176 +65,260 @@ cd $TESTCASEDIR
 usage()
 {
 	cat <<-END >&2
-	usage: ./tsstests.sh [-v <version>] [-l <logfile>] [-e <errfile>] [-ph] [<dir>]
-		This script will run all tspi-related tests.
-		-v	version of the TSS spec to use
-		-l	logfile to redirect output to (default is command line)
-		-p	check that the test cases can be run, but do not actually run them
-		-h	display this help
-		-q	run quietly - display only total number of tests passed/failed
-		-e	file name to log errors to
+	usage: ./tsstests.sh [-v <version>] [-l <logfile>] [-e <errfile>] [-d <dir>] [-h]
+		This script will run all tspi-related tests unless the <dir> option is provided.
+		-v	 version of the TSS spec to use, 1.1 or 1.2
+		-l	 logfile to redirect output to (default is command line)
+		-f	 error output format: "wiki" or "standard"
+		-h	 display this help
+		-q	 run quietly - display only total number of tests passed/failed
+		-e	 file name to log errors to
+		-d <dir> a specific directory to run tests from (to run a subset of all tests)
 	END
-	exit
+	exit -1
+}
+
+# Parse the options
+while getopts v:l:f:hqd: arg
+do
+	case $arg in
+		v)
+			export TSS_VERSION=$OPTARG
+			;;
+		l)
+			if test $QUIET -eq 1; then
+				echo "Conflicting args: -q and -l"
+				exit -1
+			fi
+			export LOGFILE="$LOGDIR/"$OPTARG""
+			LOGGING=1
+			touch $LOGFILE
+			;;
+		f)
+			OUTPUT_FORMAT=$OPTARG
+			;;
+		h)
+			usage
+			;;
+		q)
+			if test $LOGGING -eq 1; then
+				echo "Conflicting args: -q and -l"
+				exit -1
+			fi
+			QUIET=1
+			;;
+		e)
+			export ERR_SUMMARY=../../$OPTARG
+			;;
+		d)
+			if test -d ${LTPTSSROOT}/${TESTCASEDIR}/$OPTARG; then
+				SPECIFIC_TEST_DIR=$OPTARG
+				echo "Only running tests found in ${LTPTSSROOT}/${TESTCASEDIR}/$OPTARG"
+			else
+				echo "Unknown option: $OPTARG."
+				usage
+			fi
+			;;
+		?)
+			usage
+			;;
+	esac
+done
+
+echo "DEBUG DOLLARSTAR: $*"
+
+# Verify the output format
+case "$OUTPUT_FORMAT" in
+	*standard*)
+		echo "Using output format: $OUTPUT_FORMAT"
+		;;
+	*wiki*)
+		echo "Using output format: $OUTPUT_FORMAT"
+		;;
+	*)
+		echo "Unknown format: $OUTPUT_FORMAT"
+		usage
+		;;
+esac
+
+# Print the final total test run info based on output format
+# $1 = number passed
+# $2 = number failed
+# $3 = number not implemented
+# $4 = number not applicable
+# $5 = number segfaulted
+print_totals()
+{
+	case "$OUTPUT_FORMAT" in
+	*standard*)
+		echo -e "PASSED: $1\nFAILED: $2 (NOTIMPL: $3)\nNOT APPLICABLE: $4\nSEGFAULTED: $5\n" >> $ERR_SUMMARY
+		;;
+	*wiki*)
+		# Print a new header
+		echo "Total Passed | "  >> $ERR_SUMMARY
+		echo "  Total Failed |"  >> $ERR_SUMMARY
+		echo "    Total Not Implemented |"  >> $ERR_SUMMARY
+		echo "      Total Not Applicable |"  >> $ERR_SUMMARY
+		echo "        Total segfaulted"  >> $ERR_SUMMARY
+
+		# Print the final info
+		echo "$1 | "  >> $ERR_SUMMARY
+		echo "  $2 |"  >> $ERR_SUMMARY
+		echo "    $3 |"  >> $ERR_SUMMARY
+		echo "      $4 |"  >> $ERR_SUMMARY
+		echo "        $5"  >> $ERR_SUMMARY
+		;;
+	*)
+		echo "Unknown output format!"
+		exit -1
+		;;
+	esac
+}
+
+# Print the final total test run info based on output format
+# $1 = Testcase name and version string
+# $2 = The stderr from the testcase
+# $3 = The return code received from $?
+print_error()
+{
+	case "$OUTPUT_FORMAT" in
+	*standard*)
+		echo $2 >> $ERR_SUMMARY
+		;;
+	*wiki*)
+		echo "$1 | " >> $ERR_SUMMARY
+		echo "  $2 |" >> $ERR_SUMMARY
+		echo "    $3 |" >> $ERR_SUMMARY
+		echo "      . |" >> $ERR_SUMMARY
+		echo "        ." >> $ERR_SUMMARY
+		;;
+	*)
+		echo "Unknown output format!"
+		exit -1
+		;;
+	esac
+}
+
+# Print any initial output based on output format
+print_init()
+{
+	if test $INIT -eq 1; then
+		return
+	fi
+
+	case "$OUTPUT_FORMAT" in
+		*standard*)
+			;;
+		*wiki*)
+			echo "Test Name / Version | " >> $ERR_SUMMARY
+			echo "  Test Output |" >> $ERR_SUMMARY
+			echo "    Test Return Code |" >> $ERR_SUMMARY
+			echo "      . |" >> $ERR_SUMMARY
+			echo "        ." >> $ERR_SUMMARY
+			;;
+		*)
+			echo "Unknown output format!"
+			exit -1
+			;;
+	esac
+
+	INIT=1
 }
 
 
-# this is to make sure all test cases being run are current, and that make has
-# been done in all directories. this part will probably be removed later.
-#echo "<<< Compiling all test cases >>>"
-#for i in $DIRS_TO_RUN;
-#	do
-#		echo $i;
-#		cd $LTPTSSROOT/$TESTCASEDIR/$i;
-#		make;
-#	make install;
-#	done
-#echo "<<< Done compiling >>>"
-
-while getopts v:l:phq arg
-do
-	case $arg in
-		v)	export TSS_VERSION=$OPTARG
-			shift;;
-		l)	export LOGFILE="$LOGDIR/"$OPTARG""
-			let LOGGING=1
-			`touch ${LOGFILE}`
-			cat /dev/null > $LOGFILE
-			shift;;
-		p)	echo "Checking setup for testcases; not running.."
-			let PRETENDING=1;;
-		h)	usage;;
-		q)	let QUIET=1;;
-		e)	export ERR_SUMMARY=$OPTARG
-			shift
-	esac
-done
-if test $OPTIND -ne 1; then
-	shift `let OPTIND-=1`
-fi
-
-if [ x${1} != x ]; then
-	DIRS_TO_RUN=${1}
-else
-	DIRS_TO_RUN=`ls */Makefile | sed "s/Makefile//g" | sed "s/common\///g" | sed "s/highlevel\///g" | grep -v sctp`
-fi
-
-/bin/rm -f $ERR_SUMMARY
-
-
-if [ $PRETENDING -ne 0 ]; then
-	echo "Requirements for running testcases met."
-else
-#	if [ $QUIET -eq 0 ]; then
-#		echo "<<< Running all test cases >>>"
-#	fi
-#
-	for i in $DIRS_TO_RUN;
-		do
-			if [ $QUIET -eq 0 ]; then
-				echo $i;
-			fi
-			if [ $QUIET -eq 0 ]; then
-				cd ${LTPTSSROOT}/${TESTCASEDIR}/$i;
-			else
-				cd ${LTPTSSROOT}/${TESTCASEDIR}/$i &> /dev/null;
-			fi
-				FILES_TO_RUN=`ls *.c | sed "s/\.c//g"`;
-			if [ $QUIET -eq 0 ]; then
-				cd ../../bin
-			else
-				cd ../../bin &> /dev/null;
-			fi
-			for e in ${FILES_TO_RUN};
-			do
-				if [ $LOGGING -eq 0 ]; then
-					if [ $QUIET -eq 0 ]; then
-						{
-							./$e -v ${TSS_VERSION}
-							RUNRESULT=$?
-						} 2>> "$ERR_SUMMARY"
-					else
-						{
-							./$e -v ${TSS_VERSION}
-							RUNRESULT=$?
-						} &> /dev/null
-					fi
-				else
-					if [ $QUIET -eq 0 ]; then
-						{
-							./$e -v ${TSS_VERSION}
-							RUNRESULT=$?
-						} >> "$LOGFILE" 2>> "$ERR_SUMMARY"
-					else
-						{
-							./$e -v ${TSS_VERSION}
-							RUNRESULT=$?
-						} &> /dev/null
-					fi
-				fi
-				if [ $QUIET -eq 0 ]; then
-					if [ $LOGGING -eq 0 ]; then
-						echo RESULT: $RUNRESULT
-					else
-						echo RESULT: $RUNRESULT >> $LOGFILE
-					fi
-				fi
-				if [ $RUNRESULT -ne "0" ]; then
-					PASSFAIL=1;
-					if [ $RUNRESULT -gt 127 ]; then
-						let SEGFAULTED+=1;
-						let FAILED+=1;
-						echo "./$e -v ${TSS_VERSION}" >> $ERR_SUMMARY
-					elif [ $RUNRESULT -eq 127 ]; then
-						let NA+=1;
-					elif [ $RUNRESULT -eq 6 ]; then
-						let NOTIMPL+=1;
-					else
-						let FAILED+=1;
-					fi
-				else
-					let PASSED+=1;
-				fi
-				if [ $QUIET -eq 0 ]; then
-					if [ $LOGGING -eq 0 ]; then
-						echo PASSED: $PASSED \($NA N/A\)
-						echo FAILED: $FAILED \($NOTIMPL NOTIMPL\)
-						echo SEGFAULTED: $SEGFAULTED
-					else
-						{
-						echo PASSED: $PASSED
-						echo FAILED: $FAILED \($NOTIMPL NOTIMPL\)
-						echo SEGFAULTED: $SEGFAULTED
-						} >> "$LOGFILE"
-					fi
-				fi
-			done
-		done
-	if [ $QUIET -eq 0 ]; then
-		echo "<<< Test case run completed >>>"
-	fi
-
-	if [ $PASSFAIL -eq "0" ]; then
-		if [ $LOGGING -eq 0 ]; then
-			echo Passed Testcases: $PASSED
-			echo Failed Testcases: $FAILED
-			echo TSStests reported PASS
-		else
-			echo Passed Testcases: $PASSED >> $LOGFILE
-			echo Failed Testcases: $FAILED >> $LOGFILE
-			echo TSStests reported PASS >> $LOGFILE
-		fi
+# $1 = the test command line
+execute_test()
+{
+	if test $LOGGING -eq 1; then
+		# capture stderr and send stdout to the logfile
+		TEST_STDERR=$( $1 -v ${TSS_VERSION} 2>&1 >> $LOGFILE )
 	else
-		if [ $LOGGING -eq 0 ]; then
-			echo Passed Testcases: $PASSED
-			echo Failed Testcases: $FAILED \($NOTIMPL NOTIMPL\)
-			echo Segfaulted: $SEGFAULTED
-			echo TSStests reported FAIL
-		else
-			echo Passed Testcases: $PASSED >> $LOGFILE
-			echo Failed Testcases: $FAILED >> $LOGFILE
-			echo Segfaulted: $SEGFAULTED >> $LOGFILE
-			echo TSStests reported FAIL >> $LOGFILE
-		fi
+		# capture stderr and send stdout to the terminal
+		TEST_STDERR=$( $1 -v ${TSS_VERSION} 2>&1 >/dev/tty )
 	fi
-fi
+	RUNRESULT=$?
+
+	if test $RUNRESULT -ne 0; then
+		if test $RUNRESULT -gt 126; then
+			SEGFAULTED=$(( $SEGFAULTED + 1))
+			FAILED=$(( $FAILED + 1));
+		elif test $RUNRESULT -eq 126; then
+			# 126 is a special number used in testsuite/tcg/common/common.c::print_NA()
+			# and is triggered when a testcase is not applicable to the TSS version
+			# being tested
+			NA=$(( $NA + 1))
+		elif test $RUNRESULT -eq 6; then
+			NOTIMPL=$(( $NOTIMPL + 1))
+		else
+			FAILED=$(( $FAILED + 1))
+		fi
+
+		print_error "$1 -v ${TSS_VERSION}" "$TEST_STDERR" "$RUNRESULT"
+	else
+		if test $LOGGING -eq 1; then
+			echo $TEST_STDERR >> $LOGFILE
+		else
+			echo $TEST_STDERR
+		fi
+		PASSED=$(( $PASSED + 1))
+	fi
+}
+
+# main is called at the very end of this script
+# $1 = a specific directory to go to run tests
+main()
+{
+	PASSED=0
+	FAILED=0
+	SEGFAULTED=0
+	NOTIMPL=0
+	NA=0
+
+	if test x$1 != x; then
+		DIRS_TO_RUN=$1
+	else
+		DIRS_TO_RUN=`ls */Makefile | sed "s/Makefile//g" | sed "s/common\///g" | sed "s/highlevel\///g"`
+	fi
+
+	for DIRECTORY in $DIRS_TO_RUN
+	do
+		TESTS_TO_RUN=
+
+		if test $QUIET -eq 0; then
+			echo $DIRECTORY
+			cd ${LTPTSSROOT}/${TESTCASEDIR}/$DIRECTORY
+			TESTS_TO_RUN=`ls *.c | sed "s/\.c//g"`
+			cd ../../bin
+		else
+			cd ${LTPTSSROOT}/${TESTCASEDIR}/$DIRECTORY &> /dev/null
+			TESTS_TO_RUN=`ls *.c | sed "s/\.c//g"`
+			cd ../../bin &> /dev/null
+		fi
+
+		print_init
+
+		for TEST in $TESTS_TO_RUN
+		do
+			execute_test ./$TEST
+
+			# Printing totals here is a special case: if you're watching the output
+			# roll by (not going to a log file), its good to know a general pass/fail
+			# count so you can kill the run if something is obviously wrong
+			if test $QUIET -eq 0; then
+				if test $LOGGING -eq 0; then
+					echo -e "PASSED: $PASSED\nFAILED: $FAILED (NOTIMPL: $NOTIMPL)\nNOT APPLICABLE: $NA\nSEGFAULTED: $SEGFAULTED"
+				fi
+			fi
+		done
+	done
+
+	if test $QUIET -eq 0; then
+		print_totals $PASSED $FAILED $NOTIMPL $NA $SEGFAULTED
+		echo "<<< Test suite run completed >>>"
+	fi
+}
+
+main $SPECIFIC_TEST_DIR
+
+exit 0
 
