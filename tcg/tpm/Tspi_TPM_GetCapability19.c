9c32453cde3a8d23a71067dbcea369a8a7ffbfe5
kyoder <kyoder>
2007-10-16 15:54:25 +0000

    initial add -- moved in from ../highlevel/tpm

---
+++ tcg/tpm/Tspi_TPM_GetCapability19.c
@@ -0,0 +1,148 @@
+/*
+ *
+ *   Copyright (C) International Business Machines  Corp., 2004, 2005
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *	Tspi_TPM_GetCapability19.c
+ *
+ * DESCRIPTION
+ *	This test will verify that Tspi_TPM_GetCapability receives SUCCESS when returning the
+ *			maximum number of sessions supported by the TPM.
+ *
+ * ALGORITHM
+ *	Setup:
+ *		Create Context
+ *		Connect Context
+ *		Get TPM Object
+ *		Set SubCap Info
+ *
+ *	Test:
+ *		Call TPM_GetCapability then if it does not succeed
+ *		Make sure that it returns the proper return codes
+ *		Print results
+ *
+ *	Cleanup:
+ *		Free memory related to hContext
+ *		Close context
+ *
+ * USAGE
+ *      First parameter is --options
+ *                         -v or --version
+ *      Second parameter is the version of the test case to be run
+ *      This test case is currently only implemented for v1.1
+ *
+ * HISTORY
+ *      Giampaolo Libralao, glibrala@br.ibm.com - 10/2007.
+ *
+ * RESTRICTIONS
+ *	None.
+ */
+
+#include <stdio.h>
+#include "common.h"
+
+int
+main( int argc, char **argv )
+{
+	char version;
+
+	version = parseArgs( argc, argv );
+	if (version)
+		main_v1_1();
+	else
+		print_wrongVersion();
+}
+
+int
+main_v1_1( void )
+{
+	char			*function = "Tspi_TPM_GetCapability19";
+	UINT32			pulRespDataLength;
+	BYTE			*pRetData;
+	UINT32			subCap, subCapLength, numPcrs;
+	TSS_HCONTEXT		hContext;
+	TSS_HTPM		hTPM;
+	TSS_RESULT		result;
+	UINT32			exitCode;
+
+	print_begin_test( function );
+
+		// Create Context
+	result = Tspi_Context_Create( &hContext );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Create", result );
+		exit( result );
+	}
+
+		// Connect to Context
+	result = Tspi_Context_Connect( hContext, get_server(GLOBALSERVER) );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_Connect", result );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+		// Retrieve TPM object of context
+	result = Tspi_Context_GetTpmObject( hContext, &hTPM );
+	if ( result != TSS_SUCCESS )
+	{
+		print_error( "Tspi_Context_GetTpmObject", result );
+		Tspi_Context_FreeMemory( hContext, NULL );
+		Tspi_Context_Close( hContext );
+		exit( result );
+	}
+
+	subCap = TSS_TPMCAP_PROP_MAXSESSIONS;
+	subCapLength = sizeof(UINT32);
+
+	result = Tspi_TPM_GetCapability( hTPM, TSS_TPMCAP_PROPERTY,
+						subCapLength, (BYTE *)&subCap,
+						&pulRespDataLength,
+						&pRetData );
+	if ( result != TSS_SUCCESS )
+	{
+		if( !(checkNonAPI(result)) )
+		{
+			print_error( function, result );
+			exitCode = 1;
+		}
+		else
+		{
+			print_error_nonapi( function, result );
+			exitCode = 1;
+		}
+	}
+	else
+	{
+		print_success( function, result );
+		exitCode = 0;
+
+		if (pulRespDataLength == sizeof(UINT32))
+			fprintf(stderr, "\tThe maximum number of sessions supported by the TPM is %u\n\n",
+				*(UINT32 *)pRetData );
+	}
+
+	print_end_test( function );
+	Tspi_Context_FreeMemory( hContext, NULL );
+	Tspi_Context_Close( hContext );
+	exit( exitCode );
+}
