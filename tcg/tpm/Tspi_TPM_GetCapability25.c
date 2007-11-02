e2df1dcf3cb15b193616277bf9a95b1330fe88b5
kyoder <kyoder>
2007-11-02 18:59:58 +0000

    updated to be NA on a 1.1 test run

--- tcg/tpm/Tspi_TPM_GetCapability25.c
+++ tcg/tpm/Tspi_TPM_GetCapability25.c
@@ -63,14 +63,16 @@ main( int argc, char **argv )
 	char version;
 
 	version = parseArgs( argc, argv );
-	if (version)
-		main_v1_1();
+	if (version == TESTSUITE_TEST_TSS_1_2)
+		main_v1_2(version);
+	else if (version == TESTSUITE_TEST_TSS_1_1)
+		print_NA();
 	else
 		print_wrongVersion();
 }
 
 int
-main_v1_1( void )
+main_v1_2( char version )
 {
 	char			*function = "Tspi_TPM_GetCapability25";
 	UINT32			pulRespDataLength;
